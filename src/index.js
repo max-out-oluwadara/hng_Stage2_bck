84334b601c92db515c81dd79388693a4464c1b68
Oluwadara <maxegunjobi@gmail.com>
2024-07-10 08:21:09 +0100

    yes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+    res.json({ message: 'Hello World! dara' });
 });
 
 app.listen(port, async () => {
